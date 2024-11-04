3aa3f2a93536e66b7ad36ab7492cbd120cbcf43d
cnm37@naver.com <cnm37@naver.com>
2024-11-04 17:06:04 +0900

    Section3 - formLogin()

--- src/main/java/io/security/springsecurity/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/SecurityConfig.java
@@ -1,15 +1,24 @@
 package io.security.springsecurity;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
@@ -18,7 +27,32 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(form -> form
+//                        .loginPage("/loginPage")
+                        .loginProcessingUrl("/loginProc")
+                        // true -> 인증에 성공하면 무조건 "/"로 이동
+                        // false 또는 값 없음 -> 인증에 성공하면 이전 위치로 리다이렉트
+                        .defaultSuccessUrl("/", true)
+                        .failureUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        // 위에가 어떤거든 hander가 우선적으로 실행됨
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                System.out.println("authentication : " + authentication);
+                                response.sendRedirect("/home");
+                            }
+                        })
+                        .failureHandler(new AuthenticationFailureHandler() {
+                            @Override
+                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                System.out.println("exception : " + exception.getMessage());
+                                response.sendRedirect("/login");
+                            }
+                        })
+                        .permitAll()
+                );
 
         return http.build();
     }
@@ -28,6 +62,6 @@ public class SecurityConfig {
         UserDetails user = User.withUsername("user")
                 .password("{noop}1111")
                 .roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
+return new InMemoryUserDetailsManager(user);
     }
 }
