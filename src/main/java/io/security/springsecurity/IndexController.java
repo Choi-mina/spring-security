f5e2cd79f6077e5156d06f8805b4711d7a6dddf1
cnm37@naver.com <cnm37@naver.com>
2024-11-11 16:28:33 +0900

    Section4 - SecurityContext / SecurityContextHolder

--- src/main/java/io/security/springsecurity/IndexController.java
+++ src/main/java/io/security/springsecurity/IndexController.java
@@ -1,13 +1,63 @@
 package io.security.springsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    SecurityContextService securityContextService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(String customParam) {
+        SecurityContext securityContext = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        System.out.println("authentication : " + authentication);
+
+        securityContextService.securityContext();
         return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous() {
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication) {
+        // 인증받지 않은 anonymous는 authentication 객체가 null로 들어옴
+        if(authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "null";
+        }
+    }
+
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext context) {
+        // @CurrentSecurityContext를 사용하게 되면 anonymous를 객체로 받을 수 있음
+        return context.getAuthentication().getName();
+    }
+
+    @GetMapping("/logoutSuccess")
+    public String logoutSuccess() {
+        return "logoutSuccess";
     }
 }
