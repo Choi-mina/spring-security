05809ce585a4d2822b9e2c9be67c56eac8163744
cnm37@naver.com <cnm37@naver.com>
2025-01-09 00:05:36 +0900

    csrf

--- src/main/java/io/security/springsecurity/sercurity/config/SecurityConfig.java
+++ src/main/java/io/security/springsecurity/sercurity/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(restAuthenticationFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager)
                 .exceptionHandling(exception -> exception
